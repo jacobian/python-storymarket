f58891275468cfec7a91603111f2563de76252e3
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-07-09 11:36:21 -0500

    Initial structure.

---
+++ README.rst
@@ -0,0 +1,30 @@
+Python bindings to the Storymarket API
+======================================
+
+This is a cloud for the `Storymarket API <XXX>`_.
+
+You'll need a Storymarket account to use this library, and you'll need to
+generate an API token by visiting the 
+`Developer API page <http://storymarket.com/users/api/>`_.
+
+Usage
+-----
+
+First create an instance of the API with your creds::
+
+    
+Then call methods::
+        
+For details, 
+`see the documentation <http://packages.python.org/python-storymarket/>`_ 
+and/or Storymarket's `API documentation <XXX>`_.
+
+Contributing
+------------
+
+Development takes place 
+`on GitHub <http://github.com/jacobian/python-storymarket>`_; please file
+bugs/pull requests there.
+
+Development on this project was funded by the 
+`Lawrence Journal-World <http://ljworld.com/>`_ - thanks!
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    
+requirements = ['httplib2']
+if sys.version_info <= (2,6):
+    requirements.append('simplejson')
+
+setup(
+    name = "python-storymarket",
+    version = "1.0",
+    description = "Client library for the Storymarket",
+    long_description = read('README.rst'),
+    url = 'http://packages.python.org/python-storymarket',
+    license = 'BSD',
+    author = 'Jacob Kaplan-Moss',
+    author_email = 'jacob@jacobian.org',
+    packages = find_packages(exclude=['tests']),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = requirements,
+    tests_require = ["nose", "mock"],
+    test_suite = "nose.collector",
+)
