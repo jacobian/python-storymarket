ae226767e73aaa78672d6dace2f9fa2741391057
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-07-09 16:09:57 -0500

    Stubbed out orgs.

---
+++ storymarket/orgs.py
@@ -0,0 +1,31 @@
+"""
+Organizations.
+"""
+
+from __future__ import absolute_import
+
+from . import base
+
+class Org(base.Resource):
+    def __repr__(self):
+        return "<Org: %s>" % self.name
+        
+class OrgManager(base.Resource):
+    resource_class = Org
+
+    def all(self):
+        """
+        Get a list of all orgs.
+
+        :rtype: list of org instances.
+        """
+        return self._list('/orgs/')
+
+    def get(self, resource):
+        """
+        Get an individual org.
+
+        :param resource: The org instance or its ID.
+        :rtype: A list of org instances.
+        """
+        return self._get('/orgs/%s/' % (self.urlbit, base.getid(resource)))
