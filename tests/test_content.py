42619f160e72720ab5eeb911c26e1c8c00c4562a
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-07-12 15:35:07 -0500

    Added tests for content instance.delete() and .save() methods.

--- tests/test_content.py
+++ tests/test_content.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import mock
+from nose.tools import assert_equal, assert_not_equal
 from storymarket import (Audio, Data, Photo, Text, Video, Category, Org,
                          PricingScheme, RightsScheme)
 from storymarket.content import User
@@ -8,7 +10,6 @@ from .utils import (assert_isinstance, assert_list_api, assert_get_api,
                     assert_delete_api, assert_create_api, assert_update_api)
 
 sm = FakeStorymarket()
-
 
 def test_content_apis():
     test_data = [
@@ -27,6 +28,16 @@ def test_content_apis():
         'tags': 'one, two, three'
     }
 
+    def assert_instance_delete(instance, manager):
+        with mock.patch.object(manager, 'delete'):
+            instance.delete()
+            manager.delete.assert_called_with(instance)
+
+    def assert_instance_save(instance, manager):
+        with mock.patch.object(manager, 'update'):
+            instance.save()
+            manager.update.assert_called_with(instance)
+
     for (manager, cls, urlbit, extra_data) in test_data:
         list_url = 'content/%s/' % urlbit
         detail_url = 'content/%s/1/' % urlbit
@@ -36,8 +47,10 @@ def test_content_apis():
         yield assert_list_api, sm, manager.all, cls, list_url
         yield assert_get_api, sm, manager.get, cls, detail_url
         yield assert_delete_api, sm, manager.delete, cls, detail_url
+        yield assert_instance_delete, instance, manager
         yield assert_create_api, sm, manager.create, instance, post_data, list_url
         yield assert_update_api, sm, manager.update, instance, post_data, detail_url
+        yield assert_instance_save, instance, manager
         
 def test_related_resource_properties():
     test_data = [
