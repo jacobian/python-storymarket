c3529824fbebe1780959a1f70906c3ee109e2388
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-07-13 17:03:09 -0500

    Removed a FIXME that didn't need FIXING.

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -8,7 +8,6 @@ from .utils import assert_isinstance
 def test_exception_from_response():
     resp = mock.Mock()
     resp.status = 500
-    # FIXME: what do error responses actually look like?
     exc = storymarket.exceptions.from_response(resp, "Oops!")
     assert_isinstance(exc, storymarket.exceptions.StorymarketError)
     assert_equal(str(exc), 'Oops! (HTTP 500)')
